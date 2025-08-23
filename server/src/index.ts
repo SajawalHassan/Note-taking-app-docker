fb2f00508ec6b921c453804f9133758bf3a14756
sajawalhassan <sajawalhassan.1feb@gmail.com>
2025-08-23 16:53:37 +0500

    Create docker-compose file to host db, server, and client

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Example route
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+  res.send("Hello, TypeScript Express from Docker-compose!");
 });
 
 app.listen(PORT, () => {
