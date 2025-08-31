dddb9720b41c2beeecf2b3b89ad57f4c03dfb38e
sajawalhassan <sajawalhassan.1feb@gmail.com>
2025-08-31 11:17:55 +0500

    Use port 5000

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import notesRouter from "./routes/note";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -15,6 +15,6 @@ app.use(
 
 app.use("/api/notes", notesRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is now running on http://localhost:${PORT}`);
 });
